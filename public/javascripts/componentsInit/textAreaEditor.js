18d93c6427c442fac1b40e216087edd7fdd65f60
welder496 <welder496@gmail.com>
2015-08-13 14:04:52 -0300

    Componente de edição está desabilitado permitindo somente a visualização e scroll.

--- public/javascripts/componentsInit/textAreaEditor.js
+++ public/javascripts/componentsInit/textAreaEditor.js
@@ -1,4 +1,15 @@
 $('#editNota').wysihtml5({
+  "font-styles": false,
+   "emphasis": false,
+   "color": false,
+   "textAlign": false,
+   "lists": false,
+   "html": false,
+   "blockquote": false,
+   "link": false,
+   "table": false,
+   "image": false,
+   "video": false,
    "useParserRules": false
 });
 
