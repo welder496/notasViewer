12e8c68c5c480cd231c8e07439e09760a724fe07
welder496 <welder496@gmail.com>
2015-08-24 15:24:19 -0300

    Redução de telas: 		- Somente tela de pesquisa com searchForTags (views e routes); 		- Retirada da tela searchForNotas; 		- Obs: seria interesante modificar o nome de algumas variáveis....

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var rest = require('restler');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Notas' });
+});
+
+router.post('/', function(req, res, next){
+   var button = req.body.choosenButton;
+   var searchTags = req.body.searchTags;
+   rest.post("http://"+req.header('host')+"/searchForTags",
+      {
+         multipart: true,
+         data: {choosenButton: button, searchTags: searchTags}
+      })
+   .on('success', function(data,response){
+        res.send(data);
+   });
 });
 
 module.exports = router;
--- routes/searchForTags.js
+++ routes/searchForTags.js
@@ -17,7 +17,6 @@ router.get('/', function(req, res, next) {
 
 /* POST */
 router.post('/', function(req, res, next) {
-   var del = req.body.comando;
    var button = req.body.choosenButton;
    var searchTags = req.body.searchTags;
    if (button== "OR" && (typeof(searchTags) != "undefined" && searchTags)) {
@@ -43,6 +42,18 @@ router.post('/', function(req, res, next) {
             else
                res.render('searchForTags', {results: data, show: 'false'});
          })
+   } else
+   if (button=="Texto" && (typeof(searchTags) != "undefined" && searchTags)) {
+         notasRest.getNotasLike(searchTags, function(data){
+             for (var i=0; i < data.length; i++){
+                 data[i].tags = tags(data[i].tags);
+                 data[i].arquivos = arquivos(data[i].codigo,data[i].arquivos);
+             }
+             if (data.hasOwnProperty('message'))
+                res.render('searchForTags',{results: null, show: 'true', message: data.message});
+             else
+                res.render('searchForTags',{results: data, show: 'false'});
+         });
    } else {
          notasRest.getFirstNotas(function(data){
             for (var i=0; i < data.length; i++){
