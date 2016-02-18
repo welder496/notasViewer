8e072a7f72472006481ad16ee442755d3bddc079
welder496 <welder496@gmail.com>
2016-02-18 15:08:00 -0200

    notasViewer versão 1.0.29

--- public/javascripts/components/index.js
+++ public/javascripts/components/index.js
@@ -53,9 +53,11 @@
             var codigo = $(this).attr('codigo');
             var value = $(this).attr('value');
             rest.post('/documents',{codigo: codigo, value: value},function(data){
-                 rest.get(data, function(data){
-                       window.open(data,'_blank','top=200,left=200,toolbar=no,resizable=yes,scrollbars=no,width=800,height=600');
-                 });
+                   document.write(data);
+                   document.close();
+//                 rest.get(data, function(idata){
+//                       window.open(idata,'_blank','top=200,left=200,toolbar=no,resizable=yes,scrollbars=no,width=800,height=600');
+//                 });
             });
       });
 
