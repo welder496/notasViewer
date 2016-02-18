c8af024bbaa132bead35ea2de0eb8068be1dadfd
welder496 <welder496@gmail.com>
2016-02-18 13:42:28 -0200

    notasViewer versão 1.0.18

--- public/javascripts/components/index.js
+++ public/javascripts/components/index.js
@@ -53,7 +53,9 @@
             var codigo = $(this).attr('codigo');
             var value = $(this).attr('value');
             rest.post('/documents',{codigo: codigo, value: value},function(data){
-                 window.open(data,'_blank','top=200,left=200,toolbar=no,resizable=yes,scrollbars=no,width=800,height=600');
+                 var win = window.open('_blank','top=200,left=200,toolbar=no,resizable=yes,scrollbars=no,width=800,height=600');
+                 win.document.write(data);
+                 win.document.close();
             });
       });
 
