83ee677a90c5a5ecf0ca72ab488f8f61451b704c
welder496 <welder496@gmail.com>
2016-02-19 17:15:56 -0200

    notasViewer versão 1.0.68

--- public/javascripts/components/index.js
+++ public/javascripts/components/index.js
@@ -53,10 +53,7 @@
             var codigo = $(this).attr('codigo');
             var value = $(this).attr('value');
             rest.post('/documents',{codigo: codigo, value: value},function(data){
-                 var doc = window.open().document;
-                 doc.open("application/pdf","replace");
-                 doc.write(escape(data));
-                 //window.open(data,'_blank','top=200,left=200,toolbar=no,resizable=yes,scrollbars=no,width=800,height=600');
+                 window.open("data: application/octec-stream,"+encodeURIComponent(data),'_blank','top=200,left=200,toolbar=no,resizable=yes,scrollbars=no,width=800,height=600');
             });
       });
 
