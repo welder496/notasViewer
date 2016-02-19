22561d93871602f597f67ad065711d91428e4e20
welder496 <welder496@gmail.com>
2016-02-19 13:39:06 -0200

    notasViewer versão 1.0.52

--- routes/documents.js
+++ routes/documents.js
@@ -3,30 +3,6 @@ var documents = express.Router({mergeparams: true});
 var notasRest = require('notasrest');
 var netconfig = require('netconfig');
 
-documents.get('/', function(req, res, next){
-   var codigo = req.body.codigo;
-   var documento = req.body.value;
-   var message = '';
-   notasRest.getNotaByCodigo(codigo, function(data){
-      if (data.hasOwnProperty('message')){
-            message = data.message;
-            res.json({message: message, show: 'true'});
-      } else
-      if (data.hasOwnProperty('codigo')) {
-            var id = data._id;
-            notasRest.getDocument(id+'/'+documento, function(data){
-            if (data.hasOwnProperty('message')){
-                 message = data.message;
-                 res.json({message: message, show: 'true'});
-            } else {
-                 console.log(data);
-                 res.end(data);
-            }
-         });
-      }
-  });
-});
-
 documents.post('/', function(req, res, next){
    var codigo = req.body.codigo;
    var documento = req.body.value;
@@ -43,8 +19,7 @@ documents.post('/', function(req, res, next){
                  message = data.message;
                  res.json({message: message, show: 'true'});
             } else {
-                 console.log(data);
-                 res.send(data);
+                 res.send('http://'+netconfig.getHost()+':'+netconfig.getPort()+'/arquivos/'+id+'/'+documento);
             }
          });
       }
