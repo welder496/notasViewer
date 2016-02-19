19ee893081efd53dace25a30dd8c694183bda30a
welder496 <welder496@gmail.com>
2016-02-19 13:32:11 -0200

    notasViewer versão 1.0.50

--- public/javascripts/rest/rest.js
+++ public/javascripts/rest/rest.js
@@ -63,9 +63,26 @@ var rest = function($){
          return $.ajax({
             url: url,
             type: 'GET',
-         //   beforeSend: function(req){
-         //      req.setRequestHeader('Authorization', 'Basic bm90YXNWaWV3ZXI6bm90YXNWaWV3ZXI=');
-         //   },
+            success: callback,
+            data: data,
+            contentType: type
+         });
+   };
+
+   var secGet = function(url, data, callback, type){
+
+         if ( $.isFunction(data) ){
+               type = type || callback,
+               callback = data,
+               data = {}
+         };
+
+         return $.ajax({
+            url: url,
+            type: 'GET',
+            beforeSend: function(req){
+               req.setRequestHeader('Authorization', 'Basic bm90YXNWaWV3ZXI6bm90YXNWaWV3ZXI=');
+            },
             success: callback,
             data: data,
             contentType: type
@@ -77,6 +94,7 @@ var rest = function($){
       del:del,
       post:post,
       get:get,
+      secGet: secGet
    };
 
 }(jQuery);
