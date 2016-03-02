433f600a8919e8083884362c5b429592b1494ffe
welder496 <welder496@gmail.com>
2016-03-02 13:41:45 -0300

    notasViewer versão 1.0.15

--- app.js
+++ app.js
@@ -12,7 +12,9 @@ var breadcrumb = require('./routes/breadCrumb');
 var help = require('./routes/help');
 var http_auth = require('express-http-auth');
 var morgan = require('morgan');
+var restler = require('restler');
 var fs = require('fs');
+var netConfig = require('netconfig');
 
 var app = express();
 
@@ -23,6 +25,17 @@ var accessLogger = fs.createWriteStream(__dirname+'/logAccess.log', {flags: 'a'}
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+app.use(function(req,res){
+      restler.post('http://'+netconfig.getHost()+':'+netconfig.getPort()+'/Notas/usuario/login',{
+           username: encodeURIComponent('notasViewer'),
+           password: encodeURIComponent('notasViewer')
+      }).on('success', function(data,response){
+           console.log(data);
+      }).on('error', function(err, response){
+           console.log(err);
+      });
+});
 
 app.use(morgan('combined',{stream: accessLogger}));
 app.use(bodyParser.urlencoded({extended: true}));
